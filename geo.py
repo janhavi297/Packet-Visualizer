e5c8958682bf2739e1bca2fabd0652d9ba25b01f
Janhavi Chaudhari <janhavich99@gmail.com>
2026-03-15 20:16:23 +0530

    api

--- geo.py
+++ geo.py
@@ -2,15 +2,17 @@ import requests
 
 def ip_to_location(ip):
     try:
-        url = f"https://ipinfo.io/{ip}/json"
+        url = f"https://ipinfo.io/{ip}?token=d24f5561140b55"
         response = requests.get(url)
         data = response.json()
 
         city = data.get("city")
         region = data.get("region")
         country = data.get("country")
+        lat = data.get("latitude")
+        long = data.get("longitude")
 
-        return f"{city}, {region}, {country}"
+        return f"{city}, {region}, {country}, ({lat}, {long})"
 
     except Exception as e:
         return f"Location lookup failed: {e}"
